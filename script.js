ef6a3f5349fd8a302640f785fe85b228c72b11f8
Jordy Vargas <jordyvargas182@yahoo.com>
2020-06-23 23:20:24 -0600

    :tada: Tarea finalizada, queda crear un componente personalizado.

---
+++ script.js
@@ -0,0 +1,43 @@
+var x = document.querySelector("#rangoX")
+var y = document.querySelector("#rangoY");
+var size = document.querySelector("#rangoT");
+var op = document.querySelector("#rangoO");
+var list = document.getElementById("lista");
+var acept = document.getElementById("aceptar");
+var hex = document.querySelector("#hex");
+var r = document.getElementById("rangoR");
+var g = document.getElementById("rangoG");
+var b = document.getElementById("rangoB");
+var a = document.getElementById("rangoA");
+
+let color = document.getElementById("color");
+let figura = document.querySelector("section");
+
+x.addEventListener("change", function(){
+    figura.style.left = x.value + "px";
+});
+
+y.addEventListener("change", function(){
+    figura.style.top = y.value + "px";
+});
+
+size.addEventListener("change", function(){
+    figura.style.transform = "scale(" + size.value + ")";
+});
+
+op.addEventListener("change", function(){
+    figura.style.opacity = op.value;
+});
+
+acept.addEventListener("click", function(){
+    figura.className = "";
+    figura.classList.add(list.value);
+});
+
+hex.addEventListener("change", function(){
+    figura.style.backgroundColor = "#" + hex.value;
+});
+
+color.addEventListener("change", function(){
+    figura.style.backgroundColor = "rgba(" + r.value + "," + g.value + "," + b.value + "," + a.value + ")"; 
+});
